3d8bf72c5c5424fcdd8ce53c546e40ec131100e5
GA007007\Admin <thitiwas.n@g-able.com>
2022-03-20 18:17:31 +0700

    up

--- lib/childpassvaluetoparent/my_child_2.dart
+++ lib/childpassvaluetoparent/my_child_2.dart
@@ -5,16 +5,20 @@ import 'package:flutter/material.dart';
 class MyChild2 extends StatelessWidget {
   final String title;
 
-  final VoidCallback  function;
+  final VoidCallback function;
 
   MyChild2({required this.title, required this.function});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
+    return Container(
+      margin: EdgeInsets.all(5),
+      width: double.infinity,
+      child: FloatingActionButton(
+        onPressed: () {
+          function();
+        },
         child: Text(title),
-        onTap: function,
       ),
     );
   }
